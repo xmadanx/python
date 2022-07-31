273455c53c95fd75ccba8d2d95c86e302e98bb62
xmadanx <madanmn1742@gmail.com>
2022-07-31 14:55:44 +0530

    Done

---
+++ typecasting.py
@@ -0,0 +1,18 @@
+a = 1742
+b = "Madan"
+c = '143'
+print((b))
+print(type(a))
+print(type(c))
+print(int(c))
+print(type(int(c)))
+print(float(a))
+'''
+Output:
+Madan
+<class 'int'>
+<class 'str'>
+143
+<class 'int'>
+1742.0
+'''
--- variables.py
+++ variables.py
@@ -2,8 +2,15 @@ a = 1742
 b = "Madan"
 print(a)
 print(b)
+A = 143
+B = "I Love you"
+print(A)
+print(B)
 '''
+Variables are case sensitive
 Output:
 1742
 Madan
+143
+I Love you
 '''
