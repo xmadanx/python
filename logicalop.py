9a158f4271759d48b2870ce4aeb673889dee991b
xmadanx <madanmn1742@gmail.com>
2022-07-31 18:05:22 +0530

    Done

---
+++ logicalop.py
@@ -0,0 +1,24 @@
+a = 5
+
+#and returns true if both are true
+
+print(a > 2 and a < 7)    # True
+
+print(a > 6 and a < 10)   # False
+
+print(a > 10 and a < 4)   # False
+
+#or returns true if one is true
+
+print(a > 2 or a < 7)    # True
+
+print(a > 6 or a < 10)   # True
+
+print(a > 10 or a < 4)   # False
+
+#not returns false if result is true
+
+print(not(a > 10 or a < 4))   # True
+
+
+
