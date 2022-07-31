37f874d85edb7d3720a6a06c6d41a4c334f49237
xmadanx <madanmn1742@gmail.com>
2022-07-31 15:48:58 +0530

    Done

---
+++ escapechar.py
@@ -0,0 +1,12 @@
+print("It's Python")                  # It's Python
+print('It\'s Python')                 # It's Python
+
+print("It's Python.\nI love Python") 
+
+'''
+Output:
+It's Python.
+I love Python
+'''
+
+
