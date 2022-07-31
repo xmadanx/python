507f5a3b002d16d48ff94b55309992deacc24f7a
xmadanx <madanmn1742@gmail.com>
2022-07-31 16:16:39 +0530

    Done

---
+++ arithmaticop.py
@@ -0,0 +1,30 @@
+#Addition
+a = 4
+b = 2
+print(a + b)    # 6
+
+#Subtraction
+print(a - b)    # 2
+
+#Multipication
+print(a * b)    # 8
+
+#Division
+print(a / b)    # 2.0
+
+a = 5
+b = 3
+#Modulus
+print(a % b)    # 2
+
+#Exponential
+print(a ** b)   # 125
+
+a = 11
+b = 4
+#Floor Division
+print(a // b)   # 2
+
+
+
+
