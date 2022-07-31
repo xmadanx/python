f6912141e73487c5bec6d32f6284ae756443daf2
xmadanx <madanmn1742@gmail.com>
2022-07-31 14:34:11 +0530

    Done changes

--- comments.py
+++ comments.py
@@ -1,7 +1,11 @@
 #This is single line comment
 print("Hello world....")
-'''
+"""
 This 
 is 
 Multi line comment
+"""
+'''
+Output:
+Hello world....
 '''
---
+++ variables.py
@@ -0,0 +1,9 @@
+a = 1742
+b = "Madan"
+print(a)
+print(b)
+'''
+Output:
+1742
+Madan
+'''
