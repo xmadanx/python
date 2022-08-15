31a1ca96171718a16c2e95221d4ae8bc76b186b8
xmadanx <madanmn1742@gmail.com>
2022-08-15 16:15:07 +0530

    done

---
+++ forloop.py
@@ -0,0 +1,19 @@
+for i in range(6):
+	print(f"{i}.Madan")
+"""
+0.Madan
+1.Madan
+2.Madan
+3.Madan
+4.Madan
+5.Madan
+"""
+for i in range(1,6):
+	print(f"{i}.Madan")
+"""
+1.Madan
+2.Madan
+3.Madan
+4.Madan
+5.Madan
+"""
