abec99b9ad4b69ff555cd6197dcf4e8e7e1ab6bc
xmadanx <madanmn1742@gmail.com>
2022-08-15 15:58:08 +0530

    done

---
+++ ifelse.py
@@ -0,0 +1,13 @@
+a = 10
+b = 5
+
+if a < b:
+	print("B is powerfull...")
+elif a == b:
+	print("Both are equal...")
+else:
+	print("A is powerfull...")         # A is powerfull...
+
+#Oneliner
+if a > b: print("A is powerfull...")   # A is powerfull...
+
