ef9f4976310320d6c97b4d4ef8d26b3d0d599776
xmadanx <madanmn1742@gmail.com>
2022-08-15 09:47:02 +0530

    done

--- lists.py
+++ lists.py
@@ -9,11 +9,11 @@ print(a[0])                        # i
 print(a[1])                        # love
 print(a[-1])                       # python
 
-#indexing
-print(a[1:-1])                     
+#indexing                       
+print(a[1:-1])                     # ['love', 'you']
 
 #replace elements
-a[0] = "we"
+a[0] = "we"                        # 
 
 #insert elements
 a.insert(1,"all")
@@ -23,8 +23,16 @@ a.append("c")
 
 for i in a:
     print(i)
+"""
+we
+all
+love
+you
+python
+c
+"""
 
-if "python" in a:
+if "python" in a:                  # We love python
     print("We love python")
 else:
     print("Its not python")
