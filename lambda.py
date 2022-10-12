8a713bbff2032cdd1f39599754aefd5e6e564366
xmadanx <madanmn1742@gmail.com>
2022-10-12 20:21:34 +0530

    Done looping

---
+++ lambda.py
@@ -0,0 +1,8 @@
+a = lambda a : print(a + 5)
+
+a(5)            # 10
+
+def add(a):
+	print(a + 5)
+
+add(5)          # 10
---
+++ looping.py
@@ -0,0 +1,9 @@
+d = [1]
+j=0
+for i in d:
+    d.append(1)
+    j+=1
+    if j<5+1:
+        print("Love you")
+    else:
+        break
